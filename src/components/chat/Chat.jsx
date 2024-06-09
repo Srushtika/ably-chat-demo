f3f7de9863d2929b9cc7e0ea7e32268ce5d670cb
Srushtika Neelakantam <n.srushtika@gmail.com>
2024-06-09 16:46:39 +0100

    Update chat messages component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -9,6 +9,7 @@ import {
   faCamera,
   faImage,
   faMicrophone,
+  faUserNinja,
 } from "@fortawesome/free-solid-svg-icons";
 import EmojiPicker from "emoji-picker-react";
 import { useState } from "react";
@@ -37,7 +38,92 @@ function Chat() {
           <FontAwesomeIcon className="userIcon" icon={faCircleInfo} />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://images.pexels.com/photos/462023/pexels-photo-462023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+          <div className="texts">
+            <p>This is a sample chat text message</p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <FontAwesomeIcon className="userIcon" icon={faImage} />
