8ab7d232d4a039f892f3355700d8c3808754497c
Srushtika Neelakantam <n.srushtika@gmail.com>
2024-06-09 17:07:25 +0100

    Update user details component

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,44 @@
 import "./Detail.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faUserNinja } from "@fortawesome/free-solid-svg-icons";
 
 function Detail() {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <FontAwesomeIcon className="userIcon" icon={faUserNinja} />
+        <h2>Ninja</h2>
+        <p>This is a sample user description.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <FontAwesomeIcon className="userIcon" icon={faChevronDown} />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <FontAwesomeIcon className="userIcon" icon={faChevronDown} />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <FontAwesomeIcon className="userIcon" icon={faChevronDown} />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <FontAwesomeIcon className="userIcon" icon={faChevronDown} />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
